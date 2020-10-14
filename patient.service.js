e8289844fe4ff2a8cf408ec489428860b751a593
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-10-14 12:13:31 +0200

    getting started

---
+++ patient.service.js
@@ -0,0 +1,10 @@
+const {
+  initialize,
+  loggers,
+  constants,
+} = require("@asymmetrik/node-fhir-server-core");
+const { VERSIONS } = constants;
+
+module.exports.search = async (args, context) => {
+  throw new Error("Unable to locate patients");
+};
